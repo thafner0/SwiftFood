f69f9821e66ecc2efd810d776c8b7d8ea0b7f421
cabmatthew <mcaballero3@luc.edu>
2024-12-11 07:29:12 -0600

    run_tests.sh

---
+++ run_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash\
+
+SCHEME='Mastermind'\
+DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 16'\
+
+set -o pipefail && xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" -disableAutomaticPackageResolution CODE_SIGNING_ALLOWED='NO' | xcbeautify}
