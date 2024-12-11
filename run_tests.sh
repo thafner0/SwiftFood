e2a58bcf4a3ba4b9cdb9c7d699720f03b56addb2
Matthew Caballero <70122627+cabmatthew@users.noreply.github.com>
2024-12-11 07:45:53 -0600

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+SCHEME="SwiftFood"
 DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 16'
 
-set -o pipefail && xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" -disableAutomaticPackageResolution CODE_SIGNING_ALLOWED='NO' | xcbeautify
+set -o pipefail && xcodebuild test \
+  -scheme "$SCHEME" \
+  -sdk iphonesimulator \
+  -destination "$DESTINATION" \
+  -disableAutomaticPackageResolution \
+  CODE_SIGNING_ALLOWED='NO' | xcbeautify
+
