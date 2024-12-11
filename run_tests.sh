3c642821ee5f330ee2608e2ff490235da229ec73
Matthew Caballero <70122627+cabmatthew@users.noreply.github.com>
2024-12-11 07:41:56 -0600

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
-#!/bin/bash\
+#!/bin/bash
 
-SCHEME='Mastermind'\
-DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 16'\
+SCHEME='Mastermind'
+DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 16'
 
-set -o pipefail && xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" -disableAutomaticPackageResolution CODE_SIGNING_ALLOWED='NO' | xcbeautify}
+set -o pipefail && xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" -disableAutomaticPackageResolution CODE_SIGNING_ALLOWED='NO' | xcbeautify
