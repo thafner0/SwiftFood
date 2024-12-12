283f270b3e643868ede3f2be8c1b9935f5635ba3
Trevor Hafner <thafner@luc.edu>
2024-12-12 03:02:15 -0600

    fixup! Adds some random preview data to help with UI design.

--- SwiftFood/Preview Content/PreviewData.swift
+++ SwiftFood/Preview Content/PreviewData.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import SwiftData
+
+@MainActor
+let previewContainer: ModelContainer = {
+    do {
+        let container = try ModelContainer(for: MyIngredient.self, Recipe.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+        let ingredients = [MyIngredient(title: "Lemon", amount: 5, unit: "count"), MyIngredient(title: "Sugar", amount: 1, unit: "kilogram"), MyIngredient(title: "Salt", amount: 2, unit: "kilogram"), MyIngredient(title: "Rum", amount: 750, unit: "milliliters")]
+        ingredients.forEach({ container.mainContext.insert($0) })
+        return container
+    } catch {
+        fatalError("Could not load preview data: \(error).")
+    }
+}()
